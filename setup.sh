1fa0f548264777e75e39673e3346904a997f86bc
ThibaultTricard <thibault.tricard@protonmail.com>
2024-10-15 17:30:40 +0200

    setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt install git
+sudo apt install build-essential gcc libglfw3-dev libxinerama-dev libxcursor-dev libxi-dev
+sudo snap install cmake --classic
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list http://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
+sudo apt update
+sudo apt install vulkan-sdk
+
